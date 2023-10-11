a7058c66eb63f113bb25b2809602d4799f0f15b2
maxigiordani <maxigiordani05@gmail.com>
2023-10-11 00:03:06 -0300

    ultimos cambios

---
+++ js/prueba.js
@@ -0,0 +1,5 @@
+document.getElementById('hamburger').addEventListener('click', function() {
+    const navButtons = document.getElementById('navButtons');
+    navButtons.classList.toggle('open');
+  });
+  
